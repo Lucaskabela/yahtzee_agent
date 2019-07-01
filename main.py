ab031e2da43797a7f8b3df37927cfbbb7413a074
BadDecisionsAlex <alex.ameen.tx@gmail.com>
2019-06-30 22:20:56 -0500

    added pretty printing for scorecard fields

--- main.py
+++ main.py
@@ -16,6 +16,40 @@ class ScoreRound():
             'full_house', 'sm_strt', 'lg_strt', 'yahtzee', 'chance',
             'bonus']
 
+    rows_pretty = {
+        's1': ['ones', 'one', '1s', '1'],
+        's2': ['twos', 'two', '2s', '2'],
+        's3': ['threes', 'three', '3s', '3'],
+        's4': ['fours', 'four', '4s', '4'],
+        's5': ['fives', 'five', '5s', '5'],
+        's6': ['sixes', 'six', '6s', '6'],
+        'kind3': ['three of a kind', '3 of a kind'],
+        'kind4': ['four of a kind', '4 of a kind'],
+        'full_house': ['full house'],
+        'sm_strt': ['small straight', 'sm strt', 'sm straight', 'small strt',
+                     'sm straight'],
+        'lg_strt': ['large straight', 'lg strt', 'lg straight', 'large strt',
+                     'lg straight'],
+        'bonus': ['bonus yahtzee', 'yahtzee bonus']
+    }
+
+    def decode_row(field):
+        field = field.lower()
+        if field in ScoreRound.rows:
+            return field
+        for k in ScoreRound.rows_pretty.keys():
+            if field in ScoreRound.rows_pretty[k]:
+                return k
+        if not field in ScoreRound.rows:
+            print('Uhoh! "' + field + '" is not a row in our Score Card!')
+            return None
+
+    def row_pretty(field):
+        field = ScoreRound.decode_row(field)
+        if field in ScoreRound.rows_pretty.keys():
+            return ScoreRound.rows_pretty[field][0].title()
+        return field.title()
+
     def __init__(self, number=1):
         self.number = number
         for row in ScoreRound.rows:
@@ -41,9 +75,10 @@ class ScoreRound():
         return total
 
     def fill(self, field, score):
-        if not field in ScoreRound.rows:
-            print('Uhoh! "' + field + '" is not a row in our Score Card!')
-        elif self.__dict__[field] is not None:
-            print('Nice try! You\'ve already scored "' + field + '"!')
-        else:
-            self.__dict__[field] = score
+        field = ScoreRound.decode_row(field)
+        if field is not None:
+            if self.__dict__[field] is not None:
+                print('Nice try! You\'ve already scored "' +
+                      ScoreRound.row_pretty(field) + '"!')
+            else:
+                self.__dict__[field] = score
